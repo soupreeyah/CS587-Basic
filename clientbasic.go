8315b6ac427875a33d5675696fb2cee4b859a41f
Supriya Chaudhary <33657678+soupreeyah@users.noreply.github.com>
2022-03-31 16:23:06 -0700

    Add files via upload
    newer

--- clientbasic.go
+++ clientbasic.go
@@ -1,39 +1,39 @@
 package main
 
 import(
-"ethos/altEthos"
-"ethos/syscall"
-"log"
+	"ethos/altEthos"
+	"ethos/syscall"
+	"log"
 )
 
 func init() {
-        SetupMyRpcIncrementReply (incrementReply)
+	SetupMyRpcIncrementReply (incrementReply)
 }
 
 func incrementReply (count uint64) (MyRpcProcedure ) {
-        log.Printf( "myRpcClient : Received Increment Reply : %v\n" ,count)
-        return nil
-}
+	log.Printf( "myRpcClient : Received Increment Reply : %v\n" ,count)
+	return nil
+} 
 
 
 func main () {
 
-        altEthos.LogToDirectory ("test/myRpcClient" )
+	altEthos.LogToDirectory ("test/myRpcClient")
 
-        log.Printf( "myRpcClient : before call \n" )
+	log.Printf("myRpcClient : before call \n")
 
-        fd , status := altEthos.IpcRepeat( "myRpc" , "" , nil)
-        if status!= syscall.StatusOk {
-                log.Printf( " Ipc failed : %v\n" , status)
-                altEthos.Exit(status)
-        }
+	fd , status := altEthos.IpcRepeat("myRpc" , "" , nil)
+	if status!= syscall.StatusOk {
+		log.Printf( " Ipc failed : %v\n" , status)
+		altEthos.Exit(status)
+	}
 
-        call := MyRpcIncrement {}
-        status = altEthos.ClientCall(fd , &call)
-        if status!= syscall.StatusOk {
-                log.Printf( "clientCallfailed : %v\n" , status)
-                altEthos.Exit(status)
-        }
+	call := MyRpcIncrement {}
+	status = altEthos.ClientCall(fd , &call)
+	if status!= syscall.StatusOk {
+		log.Printf( "clientCallfailed : %v\n" , status)
+		altEthos.Exit(status)
+	}
 
-        log.Printf( "myRpcClient : done\n" )
+	log.Printf( "myRpcClient : done\n" )
 }
--- serverbasic.go
+++ serverbasic.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-        "ethos/syscall"
-        "ethos/altEthos"
-        "log"
+	"ethos/syscall"
+	"ethos/altEthos"
+	"log"
 )
 
 var myRpc_increment_counter uint64 = 0
 
 func init() {
-        SetupMyRpcIncrement (increment)
+	SetupMyRpcIncrement (increment)
 }
 
 func increment () (MyRpcProcedure) {
-        log.Printf( "myRpcService : called increment \n" )
-        myRpc_increment_counter++
-        return &MyRpcIncrementReply {myRpc_increment_counter}
+	log.Printf( "myRpcService : called increment \n" )
+	myRpc_increment_counter++
+	return &MyRpcIncrementReply {myRpc_increment_counter}
 }
 
 func main () {
 
-        altEthos.LogToDirectory( "test/myRpcService")
+	altEthos.LogToDirectory("test/myRpcService")
 
-        listeningFd, status:= altEthos.Advertise("myRpc")
-        if status!= syscall.StatusOk {
-        log.Printf( "Advertising service failed : %s \n" , status)
-        altEthos.Exit(status)
-        }
+	listeningFd, status:= altEthos.Advertise("myRpc")
+	if status!= syscall.StatusOk {
+		log.Printf( "Advertising service failed : %s \n" , status)
+		altEthos.Exit(status)
+	}
 
-        for {
-                _, fd , status := altEthos.Import(listeningFd)
-                if status!=syscall.StatusOk {
-                        log.Printf( "Error calling Import : %v\n" , status)
-                        altEthos.Exit(status)
-                }
+	for {
+		_, fd , status := altEthos.Import(listeningFd)
+		if status!=syscall.StatusOk {
+			log.Printf( "Error calling Import : %v\n" , status)
+			altEthos.Exit(status)
+		}	
 
-                log.Printf("myRpcService : new connection accepted \n")
+		log.Printf("myRpcService : new connection accepted \n")
 
-                t := MyRpc{}
-                altEthos.Handle (fd,&t)
-        }
+		t := MyRpc{}
+		altEthos.Handle (fd,&t)
+	}
 
 }
